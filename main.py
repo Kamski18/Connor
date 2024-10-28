349c51698c2fafadca57dbd531641c5824399538
Kamski18 <151996410+Kamski18@users.noreply.github.com>
2024-10-28 20:08:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def save(message):
     locate = text.find(target)
     after = text[locate + len(target) + 1:]
     title = after
-    title.replace(",", "\n")
+    title = title.replace(",", "\n")
     try:
         store.append(title)
         bot.edit_message_text(chat_id=message.chat.id, message_id=load, text="all work are saved Sir")
